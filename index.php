21d6e88cce9da6a16acbb2f3f391ffb9b7f26ed5
juan-ribeiro01 <juanribeiro246@gmail.com>
2025-02-25 10:51:03 -0300

    25/02

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    
+    <div class="container-fluid bg-primary text-white text-center p-5">
+        <h1>Projeto SW2</h1>
+        <p>
+            Elaborado por Guilherme Henrique de Souza
+        </p>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row">
+
+            <div class="col-sm-4"> 
+                <h1 class="bg-primary text-warning">Administrador</h1>
+                <p class="text-primary">
+                    Utilize esta opção para realizar o cadastro
+                    de novos administradores, ou edita-los.
+                </p>
+            </div>
+
+            <div class="col-sm-4"> 
+                <h1 class="bg-danger">Categorias</h1>
+                <p class="text-danger">
+                    Utilize esta opção para cadastrar as categorias 
+                    dos produtos que serão vendidos on-line.
+                </p>
+            </div>
+
+            <div class="col-sm-4"> 
+                <h1 class="bg-success">Fornecedor</h1>
+                <p class="text-success">
+                    Utilize esta opção para cadastrar os fornecedores
+                    dos produtos que serão vendidos on-line.
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+
+            <div class="col-sm-6">
+                <h1 class="bg-warning">Produtos</h1>
+                <p class="text-warning">
+                    Utilize esta opção para cadastrar os produtos
+                    que serão vendidos no site.
+                </p>
+            </div>
+
+            <div class="col-sm-6">
+                <h1 class="bg-secondary">Vendas</h1>
+                <p class="text-secondary">
+                    Utilize esta opção para visualizar as Vendas
+                    realizadas pelos clientes.
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
