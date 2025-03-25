5aae5cd65216a061a83f2fb6473168527576e052
juan-ribeiro01 <juanribeiro246@gmail.com>
2025-03-25 10:47:26 -0300

    25/03
    parte dois da aula do gui

--- administrador_cadastro.php
+++ administrador_cadastro.php
@@ -19,6 +19,7 @@
 
                 <div>
                     <?php
+                    include "conexao.php";
                     $nome = $_REQUEST["nome"];
                     $login = $_REQUEST["login"];
                     $senha = md5( $_REQUEST["senha"]);
@@ -31,7 +32,7 @@
                     $sql = "insert into administrador(nome, login, senha)
                             values (:nome, :login, :senha)";
 
-                    include "conexao.php";
+                    
                     $result = $conexao->prepare($sql); 
                     $result->bindValue(":nome", $nome);
                     $result->bindValue(":login", $login);
